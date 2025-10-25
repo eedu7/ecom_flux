f0fefc2a7773b6fe061120b0faeb12923f85f0cf
eedu7 <mueedahmad067@gmail.com>
2025-10-25 22:42:21 +0500

    feat(router): add GoRouter setup for auth screens
    - Added routes for Login, Signup, and Forgot Password screens
    - Set initial location to `/auth/login`
    - Integrated GoRouter for structured navigation

---
+++ lib/app/router.dart
@@ -0,0 +1,22 @@
+import 'package:ecom_flux/features/auth/presentation/forgot_password_screen.dart';
+import 'package:ecom_flux/features/auth/presentation/login_screen.dart';
+import 'package:ecom_flux/features/auth/presentation/signup_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final router = GoRouter(
+  initialLocation: '/auth/login',
+  routes: [
+    GoRoute(
+      path: '/auth/login',
+      builder: (context, state) => const LoginScreen(),
+    ),
+    GoRoute(
+      path: '/auth/signup',
+      builder: (context, state) => const SignupScreen(),
+    ),
+    GoRoute(
+      path: '/auth/forgot-password',
+      builder: (context, state) => const ForgotPasswordScreen(),
+    ),
+  ],
+);
